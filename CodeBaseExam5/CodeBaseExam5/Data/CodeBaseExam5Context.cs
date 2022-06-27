afc6d6e0c561433a9c6c5e44e376e327a0ac32a8
PavitraZensarB <sathuripavithra1002@gmail.com>
2022-06-27 17:46:38 +0530

    Test5

---
+++ CodeBaseExam5/CodeBaseExam5/Data/CodeBaseExam5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CodeBaseExam5.Models;
+
+namespace CodeBaseExam5.Data
+{
+    public class CodeBaseExam5Context : DbContext
+    {
+        public CodeBaseExam5Context (DbContextOptions<CodeBaseExam5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CodeBaseExam5.Models.Student> Student { get; set; }
+    }
+}
---
+++ CodeBaseExam5/CodeBaseExam5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeBaseExam5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
