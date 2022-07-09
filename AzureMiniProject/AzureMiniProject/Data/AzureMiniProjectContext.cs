4b690d40075cfcdba7c839124095a53836640ef8
PavitraZensarB <sathuripavithra1002@gmail.com>
2022-07-09 16:05:13 +0530

    Final Project

---
+++ AzureMiniProject/AzureMiniProject/Data/AzureMiniProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AzureMiniProject.Models;
+
+namespace AzureMiniProject.Data
+{
+    public class AzureMiniProjectContext : DbContext
+    {
+        public AzureMiniProjectContext (DbContextOptions<AzureMiniProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AzureMiniProject.Models.Product>? Product { get; set; }
+    }
+}
---
+++ AzureMiniProject/AzureMiniProject/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AzureMiniProject.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string? ProductName { get; set; }
+        public int Price { get; set; }
+        public string? Brand { get; set; }
+        public string? ManufactureDate { get; set; }
+        public string? ExpirationDate { get; set; }
+    }
+}
